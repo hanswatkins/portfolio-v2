8ba296872daab231335aeae0bc85c0c5e1c83a16
hanswatkins <hanswatkins@gmail.com>
2022-10-26 11:13:43 -0700

    testing form v2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,32 +3,37 @@ import React from 'react';
 const Contact = () => {
   return (
     <body>
-      <form name='contact' netlify netlify-honeypot='bot-field' hidden>
-        <input type='text' name='name' />
-        <input type='email' name='email' />
-        <textarea name='message'></textarea>
-      </form>
-
-      <form name='contact' method='post'>
-        <input type='hidden' name='form-name' value='contact' />
-        <p>
-          <label>
-            Your Name: <input type='text' name='name' />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type='email' name='email' />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name='message'></textarea>
-          </label>
-        </p>
-        <p>
-          <button type='submit'>Send</button>
-        </p>
+      <form
+        name='Portfolio_Contact'
+        method='POST'
+        data-netlify='true'
+        data-netlify-honeypot='bot-field'
+        action='/thank-you'
+      >
+        <div className='form-group'>
+          <input
+            type='text'
+            name='name'
+            placeholder='name'
+            className='form-control'
+          />
+          <input
+            type='email'
+            placeholder='email'
+            name='email'
+            className='form-control'
+          />
+          <textarea
+            name='message'
+            rows='5'
+            placeholder='message'
+            className='form-control'
+          ></textarea>
+          <div data-netlify-recaptcha='true'></div>
+        </div>
+        <button type='submit' className='submit-btn btn'>
+          send me your message
+        </button>
       </form>
     </body>
   );
