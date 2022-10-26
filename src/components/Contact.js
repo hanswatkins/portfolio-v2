b051c43d5baf4c7aedb1535ab79c0a0a20e7b568
hanswatkins <hanswatkins@gmail.com>
2022-10-26 11:19:23 -0700

    resetting form in Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,38 +3,8 @@ import React from 'react';
 const Contact = () => {
   return (
     <body>
-      <form
-        name='Portfolio_Contact'
-        method='POST'
-        data-netlify='true'
-        data-netlify-honeypot='bot-field'
-        action='submit'
-      >
-        <div className='form-group'>
-          <input
-            type='text'
-            name='name'
-            placeholder='name'
-            className='form-control'
-          />
-          <input
-            type='email'
-            placeholder='email'
-            name='email'
-            className='form-control'
-          />
-          <textarea
-            name='message'
-            rows='5'
-            placeholder='message'
-            className='form-control'
-          ></textarea>
-          <div data-netlify-recaptcha='true'></div>
-        </div>
-        <button type='submit' className='submit-btn btn'>
-          send me your message!
-        </button>
-      </form>
+      <p>Contact.js</p>
+      <p>Testing</p>
     </body>
   );
 };
