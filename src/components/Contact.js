5b08e6aba8d9c0293d36dcf4a963dd79dfbc06a5
hanswatkins <hanswatkins@gmail.com>
2022-10-26 11:06:31 -0700

    added form to contact.js for testing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,93 +1,36 @@
 import React from 'react';
-import '../stylesheets/Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      <body>
-        <link href='Contact.css' rel='stylesheet' />
+    <body>
+      <form name='contact' netlify netlify-honeypot='bot-field' hidden>
+        <input type='text' name='name' />
+        <input type='email' name='email' />
+        <textarea name='message'></textarea>
+      </form>
 
-        <div class='fcf-body'>
-          <div id='fcf-form'>
-            <h3 class='fcf-h3'>Contact us</h3>
-
-            <form
-              id='fcf-form-id'
-              class='fcf-form-class'
-              method='post'
-              action='contact-form-process.php'
-            >
-              <div class='fcf-form-group'>
-                <label for='Name' class='fcf-label'>
-                  Your name
-                </label>
-                <div class='fcf-input-group'>
-                  <input
-                    type='text'
-                    id='Name'
-                    name='Name'
-                    class='fcf-form-control'
-                    required
-                  />
-                </div>
-              </div>
-
-              <div class='fcf-form-group'>
-                <label for='Email' class='fcf-label'>
-                  Your email address
-                </label>
-                <div class='fcf-input-group'>
-                  <input
-                    type='email'
-                    id='Email'
-                    name='Email'
-                    class='fcf-form-control'
-                    required
-                  />
-                </div>
-              </div>
-
-              <div class='fcf-form-group'>
-                <label for='Message' class='fcf-label'>
-                  Your message
-                </label>
-                <div class='fcf-input-group'>
-                  <textarea
-                    id='Message'
-                    name='Message'
-                    class='fcf-form-control'
-                    rows='6'
-                    maxlength='3000'
-                    required
-                  ></textarea>
-                </div>
-              </div>
-
-              <div class='fcf-form-group'>
-                <button
-                  type='submit'
-                  id='fcf-button'
-                  class='fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block'
-                >
-                  Send Message
-                </button>
-              </div>
-
-              <div class='fcf-credit' id='fcf-credit'>
-                Simple HTML email form provided by{' '}
-                <a
-                  href='https://www.majesticform.com'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  MajesticForm
-                </a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </body>
-    </div>
+      <form name='contact' method='post'>
+        <input type='hidden' name='form-name' value='contact' />
+        <p>
+          <label>
+            Your Name: <input type='text' name='name' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type='email' name='email' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name='message'></textarea>
+          </label>
+        </p>
+        <p>
+          <button type='submit'>Send</button>
+        </p>
+      </form>
+    </body>
   );
 };
 
